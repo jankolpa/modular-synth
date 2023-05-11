c43c6a6f870461f48b7243ef12f3e36e0024a5c0
jan.kolpakov <jan-slava.kolpakov@informatik.hs-fulda.de>
2023-05-11 15:05:59 +0200

    add horizontal scroll & bug fix

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+// create grid
 var items = [
     { w: 1, h: 1, noResize: true, content: 'my first widget' },
     { w: 2, h: 1, noResize: true, content: 'another longer widget!' },
@@ -19,15 +20,83 @@ var grid = GridStack.init({
 });
 grid.load(items);
 
+
+
+// on event if user drags item to the left or right
+var isDragging = false;
+var isScrollingLeft = false;
+var isScrollingRight = false;
+grid.on('dragstart', () => {
+    isDragging = true;
+});
+grid.on('dragstop', () => {
+    isDragging = false;
+});
+
+addEventListener("mousemove", (event) => {
+    if (isDragging) {
+        var edgeSize = 80;
+        var bodyWidth = document.body.offsetWidth
+        if (event.clientX < edgeSize) {
+            if (!isScrollingLeft)
+                startScrollLeft();
+        } else if (event.clientX > (bodyWidth - edgeSize)) {
+            if (!isScrollingRight)
+                startScrollRight();
+        } else {
+            if (isScrollingLeft) {
+                stopScrollLeft();
+            }
+            if (isScrollingRight) {
+                stopScrollRight();
+            }
+        }
+    } else {
+        if (isScrollingLeft) {
+            stopScrollLeft();
+        }
+        if (isScrollingRight) {
+            stopScrollRight();
+        }
+    }
+});
+
+let scrollInterval;
+function startScrollLeft() {
+    isScrollingLeft = true;
+    scrollInterval = setInterval(() => {
+        window.scrollBy(-15, 0);
+    }, 15);
+};
+function stopScrollLeft() {
+    isScrollingLeft = false;
+    clearInterval(scrollInterval);
+};
+function startScrollRight() {
+    isScrollingRight = true;
+    scrollInterval = setInterval(() => {
+        window.scrollBy(15, 0);
+    }, 15);
+};
+function stopScrollRight() {
+    isScrollingRight = false;
+    clearInterval(scrollInterval);
+};
+
+
+
+// on resize-event
 var style = document.createElement('style');
 document.head.appendChild(style);
 
 addEventListener("resize", (event) => {
-    var gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2;
+    gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2;
     document.getElementsByClassName('grid-stack')[0].style.width = '' + gridHeight * 6 + 'px';
 
     style.innerHTML = '.grid-stack{ height: ' + 2 * gridHeight + 'px !important; min-height: ' + 2 * gridHeight + 'px !important; }' +
         '.grid-stack-item { height: ' + gridHeight + 'px !important; min-height: ' + gridHeight + 'px !important; }' +
         ' .grid-stack-item-content { height: ' + gridHeight + 'px !important; min-height: ' + gridHeight + 'px !important; }' +
         ' .grid-stack-item[gs-y="1"]:not(.ui-draggable-dragging) { top: ' + gridHeight + 'px !important; }';
+
+    grid.opts.cellHeight = gridHeight;
 });
