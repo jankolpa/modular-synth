82cf0a3ddd6ffcdb7588d529cd5de4ffeba08825
jan.kolpakov <jan-slava.kolpakov@informatik.hs-fulda.de>
2023-07-20 16:00:56 +0200

    add navbar and add-module-button

--- app/js/script.js
+++ app/js/script.js
@@ -24,7 +24,7 @@ let connectionColorID = 0
 const connectionColorAmount = 4
 
 // create grid
-let gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2
+let gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2 + 3
 document.getElementsByClassName('grid-stack')[0].style.width = '' + gridHeight * 6 + 'px'
 // eslint-disable-next-line no-undef
 const grid = GridStack.init({
@@ -104,20 +104,21 @@ xhr.onreadystatechange = function () {
   // eslint-disable-next-line eqeqeq
   if (xhr.readyState == 4 && xhr.status == '200') {
     consoleData = JSON.parse(xhr.responseText)
-    loadModule(0, 0, 0)
-    loadModule(6, 3, 0)
-    loadModule(5, 8, 0)
-    loadModule(0, 11, 0)
-    loadModule(3, 14, 0)
-    loadModule(2, 0, 1)
-    loadModule(7, 2, 1)
-    loadModule(8, 4, 1)
-    loadModule(9, 6, 1)
-    loadModule(1, 9, 1)
-    loadModule(3, 11, 1)
-    loadModule(4, 14, 1)
-    loadModule(2, 16, 1)
-    loadModule(2, 18, 1)
+    initModuleMenu()
+    loadModule(1, 0, 0)
+    // loadModule(6, 3, 0)
+    // loadModule(5, 8, 0)
+    // loadModule(0, 11, 0)
+    // loadModule(3, 14, 0)
+    // loadModule(2, 0, 1)
+    // loadModule(7, 2, 1)
+    // loadModule(8, 4, 1)
+    // loadModule(9, 6, 1)
+    // loadModule(1, 9, 1)
+    // loadModule(3, 11, 1)
+    // loadModule(4, 14, 1)
+    // loadModule(2, 16, 1)
+    // loadModule(2, 18, 1)
   }
 }
 
@@ -138,9 +139,40 @@ async function loadSynth () {
   xhr.send(null)
 }
 
+function initModuleMenu () {
+  const addModuleMenu = document.getElementsByClassName('add-module-menu')[0]
+  for (let index = 0; index < consoleData.modules.length; index++) {
+    const moduleMenuElement = document.createElement('a')
+    moduleMenuElement.innerHTML = consoleData.modules[index].name
+    moduleMenuElement.setAttribute('class', 'dropdown-item')
+    moduleMenuElement.setAttribute('href', '#')
+
+    moduleMenuElement.addEventListener('click', function () {
+      let nextSpaceY0 = 0
+      let nextSpaceY1 = 0
+      grid.engine.nodes.forEach(node => {
+        console.log()
+        if (node.y === 0 && (node.x - nextSpaceY0) < consoleData.modules[index].width && (node.x + node.w) > nextSpaceY0) {
+          nextSpaceY0 = node.x + node.w
+        } else if (node.y === 1 && (node.x - nextSpaceY1) < consoleData.modules[index].width && (node.x + node.w) > nextSpaceY1) {
+          nextSpaceY1 = node.x + node.w
+        }
+      })
+
+      if (nextSpaceY0 <= nextSpaceY1) {
+        loadModule(index, nextSpaceY0, 0)
+      } else {
+        loadModule(index, nextSpaceY1, 1)
+      }
+    })
+
+    addModuleMenu.appendChild(moduleMenuElement)
+  }
+}
+
 function loadModule (index, placeX, placeY) {
   const xhr = new XMLHttpRequest()
-  xhr.open('GET', '../app/html/' + consoleData.modules[index].name + '.html', false)
+  xhr.open('GET', '../app/html/' + consoleData.modules[index].id + '.html', false)
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       const newWidget = grid.addWidget({
@@ -154,29 +186,29 @@ function loadModule (index, placeX, placeY) {
 
       // create module
       let thisModule = null
-      const currentModuleID = '' + moduleIDCounter + '-' + consoleData.modules[index].name
+      const currentModuleID = '' + moduleIDCounter + '-' + consoleData.modules[index].id
       const moduleElement = newWidget.getElementsByClassName('module')[0]
 
       // temporary
-      if (consoleData.modules[index].module === 'VcoModule') {
+      if (consoleData.modules[index].id === 'vcoModule') {
         thisModule = new VcoModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'AudioModule') {
+      } else if (consoleData.modules[index].id === 'audioModule') {
         thisModule = new AudioModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'VcaModule') {
+      } else if (consoleData.modules[index].id === 'vcaModule') {
         thisModule = new VcaModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'MixerModule') {
+      } else if (consoleData.modules[index].id === 'mixerModule') {
         thisModule = new MixerModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'WnModule') {
+      } else if (consoleData.modules[index].id === 'wnModule') {
         thisModule = new WnModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'LfoModule') {
+      } else if (consoleData.modules[index].id === 'lfoModule') {
         thisModule = new LfoModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'VisModule') {
+      } else if (consoleData.modules[index].id === 'visModule') {
         thisModule = new VisModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'ClockModule') {
+      } else if (consoleData.modules[index].id === 'clockModule') {
         thisModule = new ClockModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'GateModule') {
+      } else if (consoleData.modules[index].id === 'gateModule') {
         thisModule = new GateModule(audioContext, moduleElement)
-      } else if (consoleData.modules[index].module === 'EnvModule') {
+      } else if (consoleData.modules[index].id === 'envModule') {
         thisModule = new EnvModule(audioContext, moduleElement)
       }
 
@@ -250,11 +282,11 @@ const style = document.createElement('style')
 document.head.appendChild(style)
 
 addEventListener('resize', (event) => {
-  gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2
+  gridHeight = (document.body.offsetHeight - document.getElementById('top-bar').offsetHeight) / 2 + 3
   document.getElementsByClassName('grid-stack')[0].style.width = '' + gridHeight * 6 + 'px'
   canvas.style.width = '' + gridHeight * 6 + 'px'
 
-  style.innerHTML = '.grid-stack{ height: ' + 2 * gridHeight + 'px !important; min-height: ' + 2 * gridHeight + 'px !important; }' +
+  style.innerHTML = '.grid-stack{ height: ' + (2 * gridHeight + 3) + 'px !important; min-height: ' + 2 * gridHeight + 'px !important; }' +
         '.grid-stack-item { height: ' + gridHeight + 'px !important; min-height: ' + gridHeight + 'px !important; }' +
         ' .grid-stack-item-content { min-height: ' + gridHeight + 'px !important; }' +
         ' .grid-stack-item[gs-y="1"]:not(.ui-draggable-dragging) { top: ' + gridHeight + 'px !important; }'
